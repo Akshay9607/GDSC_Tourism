00c52a4819358fac61b080ce567b79e0cf5e3fb0
Akshay9607 <akshay96070418@gmail.com>
2023-01-14 16:57:41 +0530

    Redux store added

--- client/src/Home/LandingPage.jsx
+++ client/src/Home/LandingPage.jsx
@@ -7,8 +7,32 @@ import React from 'react';
 import Packages from '../Components/ListOfPackages/Packages';
 import ListOfTours from '../Components/ListOfTours/ListOfTours';
 import SpecialOffer from '../Components/SpecialOffer/SpecialOffer';
+import { useDispatch } from 'react-redux';
+import { useEffect } from 'react';
+import { addTours } from '../Redux/slice';
+
+import axios from 'axios' ;
+
+
 
 const LandingPage = (props) => {
+
+  const dispatch = useDispatch() ;
+
+  const addToursToStore = async () => {
+    const response = await axios
+    .get("http://localhost:8050/api/tour/get/all")
+    .catch((err)=>{
+      console.log("Err",err) ;
+    })
+    dispatch(addTours(response.data));
+}
+
+  useEffect(()=>{
+      addToursToStore();
+  }) ;
+ 
+
 	return (
 		<>
 			<Navbar dir={props.dir} setDir={props.setDir} />
--- client/src/index.js
+++ client/src/index.js
@@ -5,10 +5,16 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 import i18n from './i18n';
 
+import { store } from './Redux/store';
+import { Provider } from 'react-redux';
+
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+   <Provider store={store}>
     <App />
+    </Provider>
   </React.StrictMode>
 );
 
