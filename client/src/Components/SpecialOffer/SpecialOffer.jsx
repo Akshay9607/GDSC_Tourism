a974f6a85b3cfa18d8179f072dbeb434048b6420
Akshay9607 <akshay96070418@gmail.com>
2023-01-14 22:52:37 +0530

    react-18-i-next API Updated

--- client/src/Components/SpecialOffer/SpecialOffer.jsx
+++ client/src/Components/SpecialOffer/SpecialOffer.jsx
@@ -3,18 +3,25 @@ import { Card, Typography } from '@mui/material';
 import { Box } from '@mui/system';
 import '../index.css';
 import SpecialOfferCard from './SpecialOfferCard';
+import { useTranslation } from 'react-i18next';
+import { useSelector } from 'react-redux';
+import { getisLangEng } from '../../Redux/slice';
 
 const SpecialOffer = () => {
+
+	const {t,i18n} = useTranslation() ;
+	const isLangEng = useSelector(getisLangEng) ;
+
 	return (
 		<>
 			<Typography variant='h2' component='h2' align='center'>
-				{' '}
-				Special
-				<span style={{ color: '#EE685F' }}> Offer </span>
+				
+				{t("special")}
+				<span style={{ color: '#EE685F' }}> {t("offer")} </span>
 			</Typography>
 			<br />
-			<Typography variant='h6' color='white' align='right'>
-				<span style={{ backgroundColor: '#EE685F', padding: '10px', borderRadius: '1rem' }}>view all</span>
+			<Typography variant='h6' color='white' align={isLangEng?'right':'left'}>
+				<span style={{ backgroundColor: '#EE685F', padding: '10px', borderRadius: '1rem' }}>{t("viewall")}</span>
 			</Typography>
 			<Box
 				display='flex'
